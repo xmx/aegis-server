1771f7db015efa6d973490d6e5fa84f65f48f83f
w <wangzhaoyun@eastmoney.com>
2024-08-01 10:38:01 +0800

    go get -u

--- jsenv/jslib/console.go
+++ jsenv/jslib/console.go
@@ -1,7 +1,13 @@
 package jslib
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"io"
+	"reflect"
+	"strconv"
 
 	"github.com/dop251/goja"
 	"github.com/dop251/goja_nodejs/require"
@@ -15,52 +21,7 @@ func Console(w io.Writer) jsvm.Loader {
 
 	return &writerConsole{
 		w: w,
-		f: new(normalFormat),
 	}
-}
-
-type writerConsole struct {
-	w  io.Writer
-	f  formatter
-	vm *goja.Runtime
-}
-
-func (p *writerConsole) Global(vm *goja.Runtime) error {
-	p.vm = vm
-	fields := map[string]any{
-		"log":   p.print,
-		"error": p.print,
-		"warn":  p.print,
-		"info":  p.print,
-		"debug": p.print,
-	}
-
-	return vm.Set("console", fields)
-}
-
-func (p *writerConsole) Require() (string, require.ModuleLoader) {
-	return "", nil
-}
-
-func (p *writerConsole) print(call goja.FunctionCall) goja.Value {
-	msg, err := p.f.format(call)
-<<<<<<< HEAD
-	if err == nil {
-		_, err = p.w.Write(msg)
-	}
-	if err != nil {
-		return p.vm.ToValue(err)
-	}
-=======
-	if err != nil {
-		return p.vm.ToValue(err)
-	}
-	if _, err = p.w.Write(msg); err != nil {
-		return p.vm.ToValue(err)
-	}
->>>>>>> 9f53d52 (go get -u)
-
-	return goja.Undefined()
 }
 
 type discardConsole struct{}
@@ -81,6 +42,110 @@ func (c *discardConsole) Require() (string, require.ModuleLoader) {
 	return "", nil
 }
 
-func (*discardConsole) discord(_ goja.FunctionCall) goja.Value {
+func (*discardConsole) discord(goja.FunctionCall) goja.Value {
 	return goja.Undefined()
+}
+
+type writerConsole struct {
+	w  io.Writer
+	vm *goja.Runtime
+}
+
+func (wc *writerConsole) Global(vm *goja.Runtime) error {
+	wc.vm = vm
+	fields := map[string]any{
+		"log":   wc.write,
+		"error": wc.write,
+		"warn":  wc.write,
+		"info":  wc.write,
+		"debug": wc.write,
+	}
+
+	return vm.Set("console", fields)
+}
+
+func (wc *writerConsole) Require() (string, require.ModuleLoader) {
+	return "", nil
+}
+
+func (wc *writerConsole) write(call goja.FunctionCall) goja.Value {
+	msg, err := wc.format(call)
+	if err == nil {
+		_, err = wc.w.Write(msg)
+	}
+	if err != nil {
+		return wc.vm.ToValue(err)
+	}
+	return goja.Undefined()
+}
+
+func (wc *writerConsole) format(call goja.FunctionCall) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	for _, val := range call.Arguments {
+		if err := wc.parse(buf, val); err != nil {
+			return nil, err
+		}
+		buf.WriteByte(' ')
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (wc *writerConsole) parse(buf *bytes.Buffer, val goja.Value) error {
+	switch {
+	case goja.IsUndefined(val), goja.IsNull(val):
+		buf.WriteString(val.String())
+		return nil
+	}
+
+	export := val.Export()
+	switch v := export.(type) {
+	case fmt.Stringer:
+		buf.WriteString(v.String())
+	case string:
+		buf.WriteString(v)
+	case int64:
+		buf.WriteString(strconv.FormatInt(v, 10))
+	case float64:
+		buf.WriteString(strconv.FormatFloat(v, 'g', -1, 64))
+	case bool:
+		buf.WriteString(strconv.FormatBool(v))
+	case []byte:
+		str := base64.StdEncoding.EncodeToString(v)
+		buf.WriteString(str)
+	case func(goja.FunctionCall) goja.Value:
+		buf.WriteString("<Function>")
+	case goja.ArrayBuffer:
+		bs := v.Bytes()
+		str := base64.StdEncoding.EncodeToString(bs)
+		buf.WriteString(str)
+	default:
+		return wc.reflectParse(buf, v)
+	}
+
+	return nil
+}
+
+func (*writerConsole) reflectParse(buf *bytes.Buffer, v any) error {
+	vof := reflect.ValueOf(v)
+	switch vof.Kind() {
+	case reflect.String:
+		buf.WriteString(vof.String())
+	case reflect.Int64:
+		buf.WriteString(strconv.FormatInt(vof.Int(), 10))
+	case reflect.Float64:
+		buf.WriteString(strconv.FormatFloat(vof.Float(), 'g', -1, 64))
+	case reflect.Bool:
+		buf.WriteString(strconv.FormatBool(vof.Bool()))
+	default:
+		tmp := new(bytes.Buffer)
+		if err := json.NewEncoder(tmp).Encode(v); err != nil || tmp.Len() == 0 {
+			return err
+		}
+		// 标准库 JSON 序列化后会在最后面加换行符，这样输出到前端会有个换行，
+		// 下面的操作就是去除 JSON 字符串后面的换行符。当然不处理也无伤大雅。
+		_, _ = buf.ReadFrom(io.LimitReader(tmp, int64(tmp.Len()-1)))
+	}
+
+	return nil
 }
