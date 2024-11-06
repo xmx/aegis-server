d804aacd7164b32f728deb05fd087654347f21cd
xmx <noreply@example.com>
2024-11-06 13:55:51 +0800

    优化协程池

--- library/gopool/pool.go
+++ library/gopool/pool.go
@@ -2,161 +2,131 @@ package gopool
 
 import (
 	"context"
-	"sync"
 	"sync/atomic"
 )
 
 type Pool interface {
-	Exec(ctx context.Context, task func(ctx context.Context))
-	Wait(parent context.Context, tasks []func(ctx context.Context)) context.Context
+	Go(fn func()) context.Context
+	Gos(ctx context.Context, fns ...func(ctx context.Context)) context.Context
 }
 
-func NewPool(maxWorkers, queue int) Pool {
-	if maxWorkers < 1 {
-		maxWorkers = 10
+func NewPool(worker, queue int) Pool {
+	if worker < 1 {
+		worker = 1
 	}
 	if queue < 0 {
-		queue = 10
+		queue = 0
 	}
 
-	return &limitExecutor{
-		sem:   make(chan struct{}, maxWorkers),
-		queue: make(chan *taskFunc, queue),
+	return &pool{
+		sema:  make(chan struct{}, worker),
+		queue: make(chan func(), queue),
 	}
 }
 
-type limitExecutor struct {
-	sem   chan struct{}
-	queue chan *taskFunc
+type pool struct {
+	sema  chan struct{}
+	queue chan func()
 }
 
-func (le *limitExecutor) Exec(ctx context.Context, task func(ctx context.Context)) {
-	if task == nil {
-		return
-	}
-	if ctx == nil {
-		ctx = context.Background()
-	}
-
-	tf := &taskFunc{ctx: ctx, task: task}
-	select {
-	case le.sem <- struct{}{}:
-		go le.exec(tf)
-		return
-	default:
-	}
-
-	select {
-	case <-ctx.Done():
-	case le.queue <- tf:
-	case le.sem <- struct{}{}:
-		go le.exec(tf)
-	}
-}
-
-func (le *limitExecutor) Wait(parent context.Context, tasks []func(ctx context.Context)) context.Context {
-	effects := make([]func(ctx context.Context), 0, len(tasks))
-	for _, task := range tasks {
-		if task != nil {
-			effects = append(effects, task)
-		}
-	}
-
-	if parent == nil {
-		parent = context.Background()
-	}
-	ctx, cancel := context.WithCancel(parent)
-	if len(effects) == 0 {
+func (p *pool) Go(fn func()) context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+	if fn == nil {
 		cancel()
 		return ctx
 	}
 
-	count := &waitCount{cancel: cancel}
-	for _, task := range effects {
-		count.incr()
-		fn := le.waitCountFunc(count, task)
-		le.Exec(ctx, fn)
+	fun := p.warpFunc(cancel, fn)
+	p.join(fun)
+
+	return ctx
+}
+
+func (p *pool) Gos(parent context.Context, fns ...func(ctx context.Context)) context.Context {
+	if parent == nil {
+		parent = context.Background()
+	}
+	ctx, cancel := context.WithCancel(parent)
+	mon := p.newMonitor(ctx, cancel)
+	funs := make([]func(), 0, len(fns))
+	for _, fn := range fns {
+		if fn == nil {
+			continue
+		}
+		fun := mon.warpFunc(fn)
+		funs = append(funs, fun)
+	}
+	if len(funs) == 0 {
+		cancel()
+		return ctx
+	}
+
+	for _, fun := range funs {
+		p.join(fun)
 	}
 
 	return ctx
 }
 
-func (le *limitExecutor) exec(tf *taskFunc) {
-	defer func() { <-le.sem }()
-
-	tf.call()
-
-	var none bool
-	for !none {
+func (p *pool) join(fun func()) {
+	select {
+	case p.sema <- struct{}{}:
+		go p.call(fun)
+	default:
 		select {
-		case tfn := <-le.queue:
-			tfn.call()
-		default:
-			none = true
+		case p.queue <- fun:
+		case p.sema <- struct{}{}:
+			go p.call(fun)
 		}
 	}
 }
 
-func (le *limitExecutor) waitCountFunc(cnt *waitCount, task func(context.Context)) func(context.Context) {
-	return func(ctx context.Context) {
-		defer func() { cnt.decr() }()
-		task(ctx)
+func (p *pool) call(fun func()) {
+	defer func() { <-p.sema }()
+
+	fun()
+	for over := false; !over; {
+		select {
+		case fun = <-p.queue:
+			fun()
+		default:
+			over = true
+		}
 	}
 }
 
-type taskFunc struct {
-	ctx  context.Context
-	task func(ctx context.Context)
+func (p *pool) warpFunc(cancel context.CancelFunc, fn func()) func() {
+	return func() {
+		defer func() {
+			recover()
+			cancel()
+		}()
+		fn()
+	}
 }
 
-func (tf *taskFunc) call() {
-	defer func() { recover() }()
-	tf.task(tf.ctx)
+func (p *pool) newMonitor(ctx context.Context, cancel context.CancelFunc) *monitor {
+	return &monitor{
+		ctx:    ctx,
+		cancel: cancel,
+	}
 }
 
-type waitCount struct {
-	count  atomic.Int64
+type monitor struct {
+	count  atomic.Int32
+	ctx    context.Context
 	cancel context.CancelFunc
 }
 
-func (ac *waitCount) incr() {
-	ac.count.Add(1)
-}
-
-func (ac *waitCount) decr() {
-	if num := ac.count.Add(-1); num <= 0 {
-		ac.cancel()
+func (m *monitor) warpFunc(fn func(ctx context.Context)) func() {
+	m.count.Add(1)
+	return func() {
+		defer func() {
+			recover()
+			if num := m.count.Add(-1); num <= 0 {
+				m.cancel()
+			}
+		}()
+		fn(m.ctx)
 	}
-}
-
-type name struct {
-	sema  chan struct{}
-	queue chan *taskFunc
-}
-
-type taskUnit struct {
-	ctx context.Context
-	fun func(ctx context.Context)
-}
-
-func (t *taskUnit) call() {
-	defer func() { recover() }()
-	t.fun(t.ctx)
-}
-
-type taskCount struct {
-	count  atomic.Int64
-	cancel context.CancelFunc
-}
-
-func (t *taskCount) incr() {
-	t.count.Add(1)
-}
-
-func (t *taskCount) decr() {
-	if num := t.count.Add(-1); num <= 0 {
-		t.cancel()
-	}
-	var wg sync.WaitGroup
-	wg.Done()
 }
