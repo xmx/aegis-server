06ac4e6938e6fb65eb7945271006f33c4e6438d2
xmx <noreply@example.com>
2024-11-07 10:01:27 +0800

    FixBUG: 自测发现存在 race condition，进行重构。

--- library/gopool/pool.go
+++ library/gopool/pool.go
@@ -6,43 +6,37 @@ import (
 )
 
 type Pool interface {
-	Go(func()) context.Context
-	Gos(context.Context, ...func(context.Context)) context.Context
+	Go(f func()) (done <-chan struct{})
+	Gos(parent context.Context, funcs ...func(parent context.Context)) (done <-chan struct{})
 }
 
-func NewPool(worker, queue int) Pool {
-	if worker < 1 {
+func NewPool(worker int) Pool {
+	if worker <= 0 {
 		worker = 1
 	}
-	if queue < 0 {
-		queue = 0
-	}
-
 	return &pool{
-		sema:  make(chan struct{}, worker),
-		queue: make(chan func(), queue),
+		sema: make(chan struct{}, worker),
 	}
 }
 
 type pool struct {
-	sema  chan struct{}
-	queue chan func()
+	sema chan struct{}
 }
 
-func (p *pool) Go(f func()) context.Context {
-	ctx, cancel := context.WithCancel(context.Background())
+func (p *pool) Go(f func()) <-chan struct{} {
+	done := make(chan struct{})
 	if f == nil {
-		cancel()
-		return ctx
+		close(done)
+		return done
 	}
 
-	fn := p.warpFunc(cancel, f)
+	fn := p.warp(done, f)
 	p.join(fn)
 
-	return ctx
+	return done
 }
 
-func (p *pool) Gos(parent context.Context, funcs ...func(ctx context.Context)) context.Context {
+func (p *pool) Gos(parent context.Context, funcs ...func(parent context.Context)) <-chan struct{} {
 	if parent == nil {
 		parent = context.Background()
 	}
@@ -50,83 +44,55 @@ func (p *pool) Gos(parent context.Context, funcs ...func(ctx context.Context)) c
 	mon := p.newMonitor(ctx, cancel)
 	fns := make([]func(), 0, len(funcs))
 	for _, f := range funcs {
-		if f == nil {
-			continue
+		if f != nil {
+			fn := mon.warp(f)
+			fns = append(fns, fn)
 		}
-		fn := mon.warpFunc(f)
-		fns = append(fns, fn)
 	}
 	if len(fns) == 0 {
 		cancel()
-		return ctx
+		return ctx.Done()
 	}
-
 	for _, fn := range fns {
 		p.join(fn)
 	}
 
-	return ctx
+	return ctx.Done()
 }
 
-func (p *pool) join(fn func()) {
-	select {
-	case p.sema <- struct{}{}:
-		go p.call(fn)
-	default:
-		select {
-		case p.queue <- fn:
-		case p.sema <- struct{}{}:
-			go p.call(fn)
-		}
-	}
+func (p *pool) join(f func()) {
+	p.sema <- struct{}{}
+	go f()
 }
 
-func (p *pool) call(fn func()) {
-	defer func() { <-p.sema }()
-
-	fn()
-	for over := false; !over; {
-		select {
-		case fn = <-p.queue:
-			fn()
-		default:
-			over = true
-		}
-	}
-}
-
-func (p *pool) warpFunc(cancel context.CancelFunc, fn func()) func() {
+func (p *pool) warp(done chan struct{}, f func()) func() {
 	return func() {
 		defer func() {
-			recover()
-			cancel()
+			close(done)
+			<-p.sema
 		}()
-		fn()
+		f()
 	}
 }
 
 func (p *pool) newMonitor(ctx context.Context, cancel context.CancelFunc) *monitor {
-	return &monitor{
-		ctx:    ctx,
-		cancel: cancel,
-	}
+	return &monitor{ctx: ctx, cancel: cancel}
 }
 
 type monitor struct {
-	count  atomic.Int32
+	cnt    atomic.Int32
 	ctx    context.Context
 	cancel context.CancelFunc
 }
 
-func (m *monitor) warpFunc(fn func(ctx context.Context)) func() {
-	m.count.Add(1)
+func (m *monitor) warp(f func(context.Context)) func() {
+	m.cnt.Add(1)
 	return func() {
 		defer func() {
-			recover()
-			if num := m.count.Add(-1); num <= 0 {
+			if num := m.cnt.Add(-1); num <= 0 {
 				m.cancel()
 			}
 		}()
-		fn(m.ctx)
+		f(m.ctx)
 	}
 }
