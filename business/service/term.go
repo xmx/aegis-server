9eba3e6f3d79ae555c7fcbffd6d4e437e5e70633
xmx <xmx@github.com>
2025-03-20 13:43:07 +0800

    添加测试样例

--- business/service/term.go
+++ business/service/term.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"crypto/ed25519"
 	"errors"
 	"io"
 	"log/slog"
@@ -16,6 +17,7 @@ import (
 	"github.com/xmx/aegis-server/business/socketerm"
 	"github.com/xmx/aegis-server/protocol/asciicast"
 	"github.com/xmx/aegis-server/protocol/vterminal"
+	"golang.org/x/crypto/ssh"
 )
 
 func NewTerm(log *slog.Logger) *Term {
@@ -60,43 +62,41 @@ func (svc *Term) PTY(conn *websocket.Conn, size *request.TermResize) error {
 	return err
 }
 
-//
-//func (svc *Term) SSH(ws *websocket.Conn, req *request.TermSSH) error {
-//	_, privateKey, err := ed25519.GenerateKey(nil)
-//	if err != nil {
-//		return err
-//	}
-//	signer, err := ssh.NewSignerFromKey(privateKey)
-//	if err != nil {
-//		return err
-//	}
-//
-//	auths := []ssh.AuthMethod{ssh.PublicKeys(signer)} // publickey 认证
-//	if req.Password != "" {
-//		auths = append(auths, ssh.Password(req.Password)) // password 认证，预先输入密码
-//	}
-//	auths = append(auths, ssh.RetryableAuthMethod(svc.keyboardInteractive(ws), 3))                    // keyboard-interactive 认证
-//	auths = append(auths, ssh.RetryableAuthMethod(ssh.PasswordCallback(svc.passwordCallback(ws)), 3)) // password 认证，后输入密码
-//
-//	sshCfg := &ssh.ClientConfig{
-//		User:            req.Username,
-//		Auth:            auths,
-//		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-//		Timeout:         time.Minute,
-//	}
-//	tty, err := vterminal.NewSSH("tcp", req.Bastion, sshCfg, req.Cols, req.Rows)
-//	if err != nil {
-//		return err
-//	}
-//	//goland:noinspection GoUnhandledErrorResult
-//	defer tty.Close()
-//
-//	castFile := filepath.Join("resources/static/play/", "ssh.cast")
-//	err = svc.serveTerminal(ws, tty, castFile)
-//
-//	return err
-//}
-//
+func (svc *Term) SSH(ws *websocket.Conn, req *request.TermSSH) error {
+	_, privateKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		return err
+	}
+	signer, err := ssh.NewSignerFromKey(privateKey)
+	if err != nil {
+		return err
+	}
+
+	auths := []ssh.AuthMethod{ssh.PublicKeys(signer)} // publickey 认证
+	if req.Password != "" {
+		auths = append(auths, ssh.Password(req.Password)) // password 认证，预先输入密码
+	}
+	auths = append(auths, ssh.RetryableAuthMethod(svc.keyboardInteractive(ws), 3))                    // keyboard-interactive 认证
+	auths = append(auths, ssh.RetryableAuthMethod(ssh.PasswordCallback(svc.passwordCallback(ws)), 3)) // password 认证，后输入密码
+
+	sshCfg := &ssh.ClientConfig{
+		User:            req.Username,
+		Auth:            auths,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         time.Minute,
+	}
+	tty, err := vterminal.NewSSH("tcp", req.Bastion, sshCfg, req.Cols, req.Rows)
+	if err != nil {
+		return err
+	}
+	//goland:noinspection GoUnhandledErrorResult
+	defer tty.Close()
+
+	castFile := filepath.Join("resources/static/play/", "ssh.cast")
+	err = svc.serveTerminal(ws, tty, castFile)
+
+	return err
+}
 
 //goland:noinspection GoUnhandledErrorResult
 func (svc *Term) serveTerminal(ws *websocket.Conn, tty vterminal.Typewriter, castFile string) error {
@@ -156,70 +156,70 @@ func (svc *Term) serveTerminal(ws *websocket.Conn, tty vterminal.Typewriter, cas
 	return nil
 }
 
-//func (svc *Term) passwordCallback(ws *websocket.Conn) func() (string, error) {
-//	conn := wsterm.NewConn(ws)
-//	return func() (string, error) {
-//		if _, err := conn.Write([]byte("请输入密码：")); err != nil {
-//			return "", err
-//		}
-//		answer, err := svc.readUntilCR(conn, false)
-//		if err != nil {
-//			return "", err
-//		}
-//		if _, err = conn.Write([]byte("\r\n")); err != nil {
-//			return "", err
-//		}
-//
-//		return answer, nil
-//	}
-//}
-//
-//func (svc *Term) keyboardInteractive(ws *websocket.Conn) ssh.KeyboardInteractiveChallenge {
-//	return func(name, instruction string, questions []string, echos []bool) ([]string, error) {
-//		conn := wsterm.NewConn(ws)
-//		answers := make([]string, 0, len(questions))
-//		size := len(echos) - 1
-//		for i, question := range questions {
-//			if _, err := conn.Write([]byte(question)); err != nil {
-//				return nil, err
-//			}
-//			var echo bool
-//			if size >= i {
-//				echo = echos[i]
-//			}
-//
-//			answer, err := svc.readUntilCR(conn, echo)
-//			if err != nil {
-//				return nil, err
-//			}
-//			answers = append(answers, answer)
-//			if _, err = conn.Write([]byte("\r\n")); err != nil {
-//				return nil, err
-//			}
-//		}
-//
-//		return answers, nil
-//	}
-//}
-//
-//func (*Term) readUntilCR(conn *wsterm.Conn, echo bool) (string, error) {
-//	var answer string
-//	for {
-//		_, data, err := conn.Input()
-//		if err != nil {
-//			return "", err
-//		}
-//		if data == "\r" {
-//			break
-//		}
-//		answer += data
-//		if !echo {
-//			continue
-//		}
-//		if _, err = conn.Write([]byte(data)); err != nil {
-//			return "", err
-//		}
-//	}
-//
-//	return answer, nil
-//}
+func (svc *Term) passwordCallback(ws *websocket.Conn) func() (string, error) {
+	conn := socketerm.New(ws, 10*time.Second)
+	return func() (string, error) {
+		if _, err := conn.Write([]byte("请输入密码：")); err != nil {
+			return "", err
+		}
+		answer, err := svc.readUntilCR(conn, false)
+		if err != nil {
+			return "", err
+		}
+		if _, err = conn.Write([]byte("\r\n")); err != nil {
+			return "", err
+		}
+
+		return answer, nil
+	}
+}
+
+func (svc *Term) keyboardInteractive(ws *websocket.Conn) ssh.KeyboardInteractiveChallenge {
+	return func(name, instruction string, questions []string, echos []bool) ([]string, error) {
+		conn := socketerm.New(ws, 10*time.Second)
+		answers := make([]string, 0, len(questions))
+		size := len(echos) - 1
+		for i, question := range questions {
+			if _, err := conn.Write([]byte(question)); err != nil {
+				return nil, err
+			}
+			var echo bool
+			if size >= i {
+				echo = echos[i]
+			}
+
+			answer, err := svc.readUntilCR(conn, echo)
+			if err != nil {
+				return nil, err
+			}
+			answers = append(answers, answer)
+			if _, err = conn.Write([]byte("\r\n")); err != nil {
+				return nil, err
+			}
+		}
+
+		return answers, nil
+	}
+}
+
+func (*Term) readUntilCR(conn *socketerm.Conn, echo bool) (string, error) {
+	var answer string
+	for {
+		_, data, err := conn.Recv()
+		if err != nil {
+			return "", err
+		}
+		if data == "\r" {
+			break
+		}
+		answer += data
+		if !echo {
+			continue
+		}
+		if _, err = conn.Write([]byte(data)); err != nil {
+			return "", err
+		}
+	}
+
+	return answer, nil
+}
